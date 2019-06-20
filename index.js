2d85cf2e81dd8017010bfcacc7fd80161f60f350
Roni <roni.colling1@gmail.com>
2019-06-20 20:58:38 +0100

    add additional routes

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ var cardSchema = mongoose.Schema({
   Name: String,
   Collection: String,
   Edition: String,
-  Colour: String
+  Colour: String,
+  spelltype: String
 });
 var MyCard = mongoose.model('Card', cardSchema)
 
@@ -30,21 +31,57 @@ app.use(function (req, res, next) {
 
   next();
 });
+
 // Routes
 app.get('/', function (req, res) {
   res.render('index', { title: 'Welcome', message: 'The MagicKeeper welcomes you' })
 })
 
-
-
 app.get('/get-all-cards', function(req, res){
   MyCard.find({},(function(err, results){
     if(err) console.log('Problem is ',err);
     res.send(200, {data: results})
+  }))
+});
+
+app.get('/cards/name/:name', function(req, res){
+  var nameToFind = req.params.name;
+  MyCard.find({Name: nameToFind}, function(err, results){
+    if(err) console.log(err)
+    res.send(200, {data: results})
   })
-)
+})
 
+app.get('/cards/colour/:colour', function(req, res){
+  var colourToFind = req.params.colour;
+  MyCard.find({Colour: colourToFind}, function(err, results){
+    if(err) console.log(err)
+    res.send(200, {data: results})
+  })
+})
 
+app.get('/cards/collection/:deck', function(req, res){
+  var deckToFind = req.params.deck;
+  MyCard.find({Collection: deckToFind}, function(err, results){
+    if(err) console.log(err)
+    res.send(200, {data: results})
+  })
+});
+
+app.get('/cards/type/:spelltype', function(req, res){
+  var typeToFind = req.params.spelltype;
+  MyCard.find({spelltype: typeToFind}, function(err, results){
+    if(err) console.log(err)
+    res.send(200, {data: results})
+  })
+})
+
+app.get('/cards/creatures/:creaturetype', function(req, res){
+  var typeToFind = req.params.creaturetype;
+  MyCard.find({creaturetype: typeToFind}, function(err, results){
+    if(err) console.log(err)
+    res.send(200, {data: results})
+  })
 })
 
 const port = process.env.PORT || 3000
