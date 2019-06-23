79139408f8e34ff84a30d9c8bac4f5dafaaf58c2
Roni <roni.colling1@gmail.com>
2019-06-23 20:25:24 +0100

    add simple stuff to update frontend

--- index.js
+++ index.js
@@ -1,87 +1,120 @@
-const express = require('express');
+const express = require('express')
 
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-const app = express();
+const bodyParser = require('body-parser')
+
+const app = express()
 
 // Tell our app where to look for view templates
-app.set('views',__dirname + '/views');
-app.set('view engine','pug');
+app.set('views',__dirname + '/views')
+app.set('view engine','pug')
 
 // Tell app where to look for the relevant files
 app.use(express.static(__dirname + '/public'))
 
-// Config
-var config = require('./config');
+// allow for parsing of request bodies
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
-var dbMethods = require('./backend/mongoose');
-var db = dbMethods.connectToDB(config);
+// Config
+var config = require('./config')
+
+var dbMethods = require('./backend/mongoose')
+var db = dbMethods.connectToDB(config)
 var cardSchema = mongoose.Schema({
-  Name: String,
-  Collection: String,
-  Edition: String,
-  Colour: String,
-  spelltype: String
-});
+	Name: String,
+	Collection: String,
+	Edition: String,  
+	Colour: String,
+	spelltype: String,
+	creaturetype: String
+})
 var MyCard = mongoose.model('Card', cardSchema)
 
 app.use(function (req, res, next) {
-  // pass db in request to all other handlers
-  req.db = db;
+	// pass db in request to all other handlers
+	req.db = db
+	next()
+})
 
-  next();
-});
-
-// Routes
+// Routes - GET
 app.get('/', function (req, res) {
-  res.render('index', { title: 'Welcome', message: 'The MagicKeeper welcomes you' })
+	res.render('index', { 
+		title: 'MagicKeeper',
+		message: 'A simple UI to import cards for storage in db',
+		listItems: [
+			{item: 'Home', href: ""},
+			{item: 'View All Cards', href: '/get-all-cards'},
+			{item: 'Amend Cards', href: ""},
+			{item: 'Update Card Info', href: ""}
+		] 
+	})
 })
 
 app.get('/get-all-cards', function(req, res){
-  MyCard.find({},(function(err, results){
-    if(err) console.log('Problem is ',err);
-    res.send(200, {data: results})
-  }))
-});
+	MyCard.find({},(function(err, results){
+		if(err) console.log('Problem is ',err)
+		res.status(200).send({data: results.length})
+	}))
+})
 
 app.get('/cards/name/:name', function(req, res){
-  var nameToFind = req.params.name;
-  MyCard.find({Name: nameToFind}, function(err, results){
-    if(err) console.log(err)
-    res.send(200, {data: results})
-  })
+	var nameToFind = req.params.name
+	MyCard.find({Name: nameToFind}, function(err, results){
+		if(err) console.log(err)
+		res.status(200).send({data: results})
+	})
 })
 
 app.get('/cards/colour/:colour', function(req, res){
-  var colourToFind = req.params.colour;
-  MyCard.find({Colour: colourToFind}, function(err, results){
-    if(err) console.log(err)
-    res.send(200, {data: results})
-  })
+	var colourToFind = req.params.colour
+	MyCard.find({Colour: colourToFind}, function(err, results){
+		if(err) console.log(err)
+		res.status(200).send({data: results})
+	})
 })
 
 app.get('/cards/collection/:deck', function(req, res){
-  var deckToFind = req.params.deck;
-  MyCard.find({Collection: deckToFind}, function(err, results){
-    if(err) console.log(err)
-    res.send(200, {data: results})
-  })
-});
+	var deckToFind = req.params.deck
+	MyCard.find({Collection: deckToFind}, function(err, results){
+		if(err) console.log(err)
+		res.status(200).send({data: results})
+	})
+})
 
 app.get('/cards/type/:spelltype', function(req, res){
-  var typeToFind = req.params.spelltype;
-  MyCard.find({spelltype: typeToFind}, function(err, results){
-    if(err) console.log(err)
-    res.send(200, {data: results})
-  })
+	var typeToFind = req.params.spelltype
+	MyCard.find({spelltype: typeToFind}, function(err, results){
+		if(err) console.log(err)
+		res.status(200).send({data: results})
+	})
 })
 
 app.get('/cards/creatures/:creaturetype', function(req, res){
-  var typeToFind = req.params.creaturetype;
-  MyCard.find({creaturetype: typeToFind}, function(err, results){
-    if(err) console.log(err)
-    res.send(200, {data: results})
-  })
+	var typeToFind = req.params.creaturetype
+	MyCard.find({creaturetype: typeToFind}, function(err, results){
+		if(err) console.log(err)
+		res.status(200).send({data: results})
+	})
+})
+
+// Routes - POST
+app.post('/add/card', function(req, res){
+	MyCard.create({Name: req.body.Name, Collection: req.body.Collection, Colour: req.body.Colour}, function(err, results){
+		if(err) console.log(err)
+		res.status(200).send({inserted: results})
+	}) 
+})
+
+app.post('/add/collection', function(req, res){
+	// as its a post request it gets passed in the body not the params
+	var stuffToInsert = require(req.body.deckname);
+	
+	MyCard.insertMany(stuffToInsert, function(err, results){
+		if(err) console.log(err)
+		res.status(200).send({inserted: results})
+	})
 })
 
 const port = process.env.PORT || 3000
---
+++ spec/index.spec.js
@@ -0,0 +1,9 @@
+const index = require('../index');
+describe('index.js', () => {
+    it('should exist', () => {
+        expect(index).toBeTruthy();
+    })
+})
+
+
+
